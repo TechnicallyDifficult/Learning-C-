aa55dbc9bed517d9bb9a61da3791aeea656cb463
Lilith Grimaude <jacobmonn@sbcglobal.net>
2020-08-19 15:58:42 -0700

    Initial commit

---
+++ GuessTheNumber/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace GuessTheNumber
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        	Console.WriteLine("Echo");
+            Console.WriteLine(Console.ReadLine());
+        }
+    }
+}
