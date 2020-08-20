5e2d7e714a7ba397216048377b9bc2348f00ecb6
Lilith Grimaude <jacobmonn@sbcglobal.net>
2020-08-19 21:48:57 -0700

    Initizlize BankAccount

--- classes/Program.cs
+++ classes/Program.cs
@@ -2,11 +2,12 @@
 
 namespace classes
 {
-    class Program
-    {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("Hello World!");
-        }
-    }
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			var account = new BankAccount("<name>", 1000);
+			Console.WriteLine($"Account {account.Number} was created for {account.Owner} with {account.Balance} initial balance.");
+		}
+	}
 }
